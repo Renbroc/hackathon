672eef375260e80c76792f671ea7270edf67fe65
Chris Alan Williams <chris@enactdevelopment.com>
2015-07-12 11:29:04 -0400

    ren_user for db

--- renbroc/__init__.py
+++ renbroc/__init__.py
@@ -10,7 +10,7 @@ from nltk import *
 app = Flask(__name__, static_url_path='/static')
 
 # Edit database connection below
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/renbroc?charset=utf8&use_unicode=1';
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://ren_user:ren_pass@localhost/renbroc?charset=utf8&use_unicode=1';
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 config = os.getenv('CONFIG_OBJECT', 'LocalConfig')
